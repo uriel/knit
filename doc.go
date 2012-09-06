868b699fa5cffd21b3c80aafa238969df7cfe864
jimt <jimteeuwen@gmail.com>
2012-09-06 17:32:41 +0200

    Adds a usage example to doc.go.

--- doc.go
+++ doc.go
@@ -4,15 +4,20 @@
 /*
 Knit parses knitting patterns into a parse tree.
 
-To make this work reliably, we define a formal and relatively
+To make this work reliably, we define a simple and relatively
 strict pattern syntax. There are established knitting pattern formats
 out there, but they are not strict enough to be suitable for parsing
 in a practical and efficient manner.
 
-In order to find the middle ground and not to inconvenience the user too
-much by requiring them to learn a new pattern language, we incorporate as
-many of the established pattern practises as possible, while still
-maintaining reliability and effectiveness in an automated parsing
-environment like this package.
+The pattern constructs we do support are taken from the overview on
+[craftyarncouncil.com](http://www.craftyarncouncil.com/knit.html).
+
+Usage example:
+
+	pat, err := knit.Parse("MyPattern", "[P3 K3] 10")
+
+or:
+
+	pat := knit.MustParse("MyPattern", "[P3 K3] 10")
 */
 package knit
